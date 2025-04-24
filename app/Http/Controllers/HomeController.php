04f975ba19ab265c6c874d94c97dd295e73899ce
unknown <anupshk39@gmail.com>
2025-04-25 00:05:09 +0545

    Notification Application

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Notifications\WelcomeNotification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $users = User::all();
+
+        $post = [
+            'title'=>'post title',
+            'slug'=>'post-slug',
+        ];
+
+        try{
+            foreach($users as $user){
+                $user->notify(new WelcomeNotification($post));
+            }
+          
+            dd("done");
+        }catch(\Exception $e){
+            dd($e->getMessage());
+        }
+
+        return view('welcome');
+    }
+}
