45317f6ee1f3cafab5591d94cd41d61a217a2f64
Anup Shakya <dev2@krizmatic.com>
2025-04-30 18:16:22 +0545

    Typeform

--- app/Http/Controllers/TypeFormController.php
+++ app/Http/Controllers/TypeFormController.php
@@ -7,26 +7,352 @@ use Illuminate\Support\Facades\Http;
 
 class TypeFormController extends Controller
 {
-    public function index(){
-        return view('typeform.index');
+    public function index()
+    {
+        $path = public_path('build/assets/countries_state.json');
+        $json = file_get_contents($path);
+        $countryStates = json_decode($json, true);
+
+        return view('typeform.index', compact('countryStates'));
     }
 
-    public function getForm($formId){
+    public function getForm($formId)
+    {
         $token = config('services.typeform.access_token');
-        
+
         $response = Http::withToken($token)->get("https://api.typeform.com/forms/{$formId}");
-        
-        if($response->successful()){
+
+        if ($response->successful()) {
             return $response->json();
         }
 
         return response()->json([
-            'error'=>'Unable to Fetch Form',
-            'details'=>$response->json()
-        ],$response->status());
+            'error' => 'Unable to Fetch Form',
+            'details' => $response->json()
+        ], $response->status());
     }
 
-    public function edit(Request $request){
-        
+    // public function edit(Request $request){
+    //     $formData = json_decode($request->typeform_data);
+    //     $selectedCountries = $request->country;
+
+    //     $countriesDetails = public_path('build/assets/countries_state.json');
+    //     $json = file_get_contents($countriesDetails,true);
+    //     $countriesStates = json_decode($json,true);
+
+    //     $formId = $formData->id;
+    //     $accessToken = config('services.typeform.access_token');
+
+    //     $fields = $formData->fields;
+
+    //     $countryLists = [];
+
+    //     foreach($selectedCountries as $selectedCountry){
+    //         foreach($countriesStates as $countriesState){
+    //             if(isset($countriesState['code2']) && $countriesState['code2'] === $selectedCountry){
+    //                 $countryLists[] = $countriesState;
+    //                 break;
+    //             }
+    //         }
+    //     }
+
+    //     $countryFieldLists = [];
+
+    //     foreach($countryLists as $countryList){
+    //         $countryFieldLists[] = [
+    //             'label'=>$countryList['name']
+    //         ];
+    //     }
+
+    //     $form_country_fields = [
+    //         "ref"=> "country_field_ref",
+    //         "title"=> "Which country are you from?",
+    //         "type"=>"dropdown",
+    //         "properties"=>[
+    //             "choices"=> $countryFieldLists
+    //         ]
+    //     ];
+
+    //     $final_array = array_merge($fields,[$form_country_fields]);
+
+    //     dd($final_array);
+
+    //     // $
+    //     // $formUpdateData = [
+    //     //     'title' => ,
+    //     //     'fields' => [
+    //     //         [
+
+    //     //         ]
+    //     //     ]
+    //     // ]
+    // }
+
+    // public function edit(Request $request){
+    //     $formData = json_decode($request->typeform_data,true);
+    //     $formId = $formData['id'];
+
+    //     $selectedCountries = $request->country;
+    //     $countriesState = json_decode(file_get_contents(public_path('build/assets/countries_state.json')),true);
+    //     $accessToken = config('services.typeform.access_token');
+
+    //     $fields = [];
+    //     $logic = [];
+    //     $logicCountryState = [];
+
+    //     $countryChoices = array_map(fn($country) => ['label'=>$country] ,$selectedCountries);
+
+    //     $fields[] = [
+    //         "ref"=>"country_field_ref",
+    //         "title"=>"Which country are your from?",
+    //         "type"=>"dropdown",
+    //         "properties"=>[
+    //             "choices"=>$countryChoices
+    //         ],
+    //     ];
+
+
+    //     foreach($selectedCountries as $country){
+    //         $countryData = collect($countriesState)->firstWhere('name',$country);
+
+    //         if(empty($countryData) || empty($countryData['states'])){
+    //             continue;
+    //         }
+
+    //         $stateFieldRef = preg_replace('/[^a-z0-9_\-]/','_',strtolower($country).'_state_field_ref');
+
+    //         $stateField = [
+    //             "ref"=>$stateFieldRef,
+    //             "title"=>"Which state are your from? ($country)",
+    //             "type"=>"dropdown",
+    //             "properties"=>[
+    //                 "choices"=>collect($countryData['states'])->map(fn($state) => ['label'=>$state['name']])->values()->all()
+    //             ],
+    //         ];
+
+    //         $fields[] = $stateField;
+
+    //         //Logic for State according to Country
+    //         $logicCountryState[] = [
+    //             "action"=>"jump",
+    //             "condition"=>[
+    //                 "op"=>"equal",
+    //                 "vars"=>[
+    //                     ["type"=>"field","value"=>"country_field_ref"],
+    //                     ["type"=>"constant","value"=>$country],
+    //                 ]
+    //             ],
+    //             "details"=>[
+    //                 "to"=>[
+    //                     "type"=>"field",
+    //                     "value"=>$stateFieldRef,
+    //                 ]
+    //             ]
+    //         ];
+
+    //         //Logic Redirecting to Thank You Field After Select State
+    //         $logic[] = [
+    //             "type"=>"field",
+    //             "ref"=>$stateFieldRef,
+    //             "actions"=>[
+    //                 [
+    //                     "action"=>"jump",
+    //                     "condition"=>[
+    //                         "op"=>"always",
+    //                         "vars"=>[],
+    //                     ],
+    //                     "details"=>[
+    //                         "to"=>[
+    //                             "type"=>"thankyou",
+    //                             "value"=>"default_tys"
+    //                         ]
+    //                     ]
+    //                 ],
+    //             ]
+    //         ];
+    //     }
+
+    //     $logic[] = [
+    //         "type"=>"field",
+    //         "ref"=>"country_field_ref",
+    //         "actions"=>$logicCountryState
+    //     ];
+
+    //     $formData['fields'][] = $fields;
+    //     $formData['logic'][] = $logic;
+    //     // $formStructure = [
+    //     //     "title"=>$formData['title'],
+    //     //     "fields"=>$fields,
+    //     //     "logic"=>$logic,
+    //     // ];
+
+
+    //     // $typeformPayload = json_encode($formStructure,JSON_PRETTY_PRINT);
+
+    //     $updateForm = Http::withToken($accessToken)->put("https://api.typeform.com/forms/{$formId}",$formData);
+
+    //     dd($updateForm->json());
+    // }
+
+    public function edit(Request $request)
+    {
+        $formData = json_decode($request->typeform_data, true);
+
+        $hasCountryField = collect($formData['fields'])->contains(function ($field) {
+            return isset($field['ref']) && $field['ref'] === 'country_field_ref';
+        });
+
+        if (!$hasCountryField) {
+            try {
+                $formId = $formData['id'];
+                $formGender = $formData['fields'][1]['ref'];
+                $formAfterGender = $formData['fields'][2]['ref'];
+
+                $selectedCountries = $request->country;
+                $countriesState = json_decode(file_get_contents(public_path('build/assets/countries_state.json')), true);
+                $accessToken = config('services.typeform.access_token');
+
+                $fields = [];
+                $logic = [];
+                $logicCountryState = [];
+
+                $countryChoices = array_map(fn($country) => ['label' => $country], $selectedCountries);
+
+                $fields[] = [
+                    "ref" => "country_field_ref",
+                    "title" => "Which country are your from?",
+                    "type" => "dropdown",
+                    "properties" => [
+                        "choices" => $countryChoices
+                    ],
+                ];
+
+
+                foreach ($selectedCountries as $country) {
+                    $countryData = collect($countriesState)->firstWhere('name', $country);
+
+                    if (empty($countryData) || empty($countryData['states'])) {
+                        continue;
+                    }
+
+                    $stateFieldRef = preg_replace('/[^a-z0-9_\-]/', '_', strtolower($country) . '_state_field_ref');
+
+                    $stateField = [
+                        "ref" => $stateFieldRef,
+                        "title" => "Which state are your from? ($country)",
+                        "type" => "dropdown",
+                        "properties" => [
+                            "choices" => collect($countryData['states'])->map(fn($state) => ['label' => $state['name']])->values()->all()
+                        ],
+                    ];
+
+                    $fields[] = $stateField;
+
+                    //Logic for State according to Country
+                    $logicCountryState[] = [
+                        "action" => "jump",
+                        "condition" => [
+                            "op" => "equal",
+                            "vars" => [
+                                ["type" => "field", "value" => "country_field_ref"],
+                                ["type" => "constant", "value" => $country],
+                            ]
+                        ],
+                        "details" => [
+                            "to" => [
+                                "type" => "field",
+                                "value" => $stateFieldRef,
+                            ]
+                        ]
+                    ];
+
+                    //Logic Redirecting to Thank You Field After Select State
+                    $logic[] = [
+                        "type" => "field",
+                        "ref" => $stateFieldRef,
+                        "actions" => [
+                            [
+                                "action" => "jump",
+                                "condition" => [
+                                    "op" => "always",
+                                    "vars" => [],
+                                ],
+                                "details" => [
+                                    "to" => [
+                                        "type" => "field",
+                                        "value" => "$formAfterGender"
+                                    ]
+                                ]
+                            ],
+                        ]
+                    ];
+                }
+
+
+                $logic[] = [
+                    "type" => "field",
+                    "ref" => $formGender,
+                    "actions" => [
+                        [
+                            "action" => "jump",
+                            "condition" => [
+                                "op" => "always",
+                                "vars" => [],
+                            ],
+                            "details" => [
+                                "to" => [
+                                    "type" => "field",
+                                    "value" => "country_field_ref",
+                                ]
+                            ]
+                        ],
+                    ]
+                ];
+
+                $logic[] = [
+                    "type" => "field",
+                    "ref" => "country_field_ref",
+                    "actions" => $logicCountryState
+                ];
+
+                $formFields = $formData['fields'];
+                $formData['fields']=[];
+                
+                foreach($formFields as $key=>$formField){
+                    if($key == 0 || $key ==1){
+                        $formData['fields'][] = $formField;
+                    }
+                }
+                
+                foreach ($fields as $field) {
+                    $formData['fields'][] = $field;
+                }
+
+                foreach($formFields as $key=>$formField){
+                    if($key > 2){
+                        $formData['fields'][] = $formField;
+                    }
+                }
+
+                foreach ($logic as $condition) {
+                    $formData['logic'][] = $condition;
+                }
+                // $formFields['logic'][] = $formFields;
+
+                // $typeformPayload = json_encode($formStructure,JSON_PRETTY_PRINT);
+
+                $updateForm = Http::withToken($accessToken)->put("https://api.typeform.com/forms/{$formId}", $formData);
+                dd($updateForm->json());
+                if($updateForm->successful()){
+                    return 'Succesfully Added Country and State Fields';
+                }else{
+                    return "Fail to Add Field";
+                }
+            }catch(\Exception $e){
+                return $e->getMessage();
+            }
+        } else {
+            return "Already Exists";
+        }
     }
 }
--- resources/views/typeform/index.blade.php
+++ resources/views/typeform/index.blade.php
@@ -1,4 +1,18 @@
 @extends('typeform.layout.web')
+@push('style')
+<style>
+    .select2-container--default .select2-selection--single{
+        height: 35px;
+    }
+    .select2-container--default .select2-selection--single .select2-selection__rendered{
+        height: 35px;
+    }
+    .select2-container--default .select2-selection--single .select2-selection__arrow{
+        height: 35px;
+    }
+</style>
+@endpush
+
 @section('content')
     <div class="card p-3 mt-4">
         <h4>Typeform Add Country and State</h4>
@@ -10,9 +24,22 @@
                 <span id="form_message"></span>
             </div>
             <div id="form_details">
+                <input type="hidden" name="typeform_data" id="typeform_data">
+                <div class="my-2">
+                    <label for="country" class="form-label">Country</label><span class="text-danger">*</span>
+                    <select id="country" name="country[]" class="form-control select2" style="height: 20px;" multiple> 
+                        <option value="all">Select All</option>
+                        @foreach($countryStates as $countryState)
+                        <option value="{{$countryState['name']}}">{{$countryState['name']}}</option>
+                        @endforeach
+                    </select>
+                </div>
             </div>      
-            <button type="submit" class="btn btn-primary float-end mx-1" disabled>Add Country & State</button>
-            <button type="button" class="btn btn-primary float-end mx-1" id="sync_form">Sync Form</button>
+            <div class="my-3">
+                <button type="submit" class="btn btn-primary float-end mx-1" id="addBtn" disabled>Add Country & State</button>
+                <button type="button" class="btn btn-primary float-end mx-1" id="sync_form">Sync Form</button>
+            </div>
+           
         </form>
     </div>
 @endsection
@@ -28,13 +55,37 @@
                     url: baseUrl+'/'+formId,
                     type:'GET',
                     success: function(response){
-                        console.log(response);
+                        $('#form_message').text('Successfully Sync!!!');
+                        $('#form_message').removeClass('text-danger');
+                        $('#form_message').addClass('text-success');
+                        $('#formId').attr('disabled',true);
+                        $('#typeform_data').val(JSON.stringify(response));
+                        $('#addBtn').attr('disabled',false);
                     },
                     error: function(err){
-                        console.log(err);
+                        $('#form_message').text('Failed to Sync');
+                        $('#form_message').removeClass('text-success');
+                        $('#form_message').addClass('text-danger');
+                        $('#formId').val('');
+                        $('#formId').attr('disabled',false);
+                        $('#addBtn').attr('disabled',false);
                     }
                 })
-            })
+            });
+
+            $('#country').on('change',function(e){
+                let selected = $(this).val();
+
+                if(selected && selected.includes('all')){
+                    let all = [];
+                    $('#country option').each(function(){
+                        let val = $(this).val();
+                        if(val !== 'all') all.push(val);
+                    });
+
+                    $('#country').val(all).trigger('change');
+                }
+            });
         })
     </script>
 @endpush
